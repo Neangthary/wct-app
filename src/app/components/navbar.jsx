89b68cc82ff38565fe6d96badc2e3192911bd9fa
Neth Wandara <ytdare51@gmail.com>
2023-10-23 13:36:21 +0700

    added some pages and components

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <a href="#" className="text-white font-bold text-2xl">Event Navigator</a>
+          <ul className="flex space-x-4">
+            <li><a href="#" className="text-white">Home</a></li>
+            <li><a href="#" className="text-white">About</a></li>
+            <li><a href="#" className="text-white">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
